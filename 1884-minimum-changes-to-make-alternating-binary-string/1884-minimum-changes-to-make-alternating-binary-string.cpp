6e7cd9a1d2b8e67568cbfb003e8e501931c44c77
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2023-09-30 22:12:41 +0530

    Time: 3 ms (81.88%), Space: 7.2 MB (35.54%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int n = s.size(), ans = 0;
-        for(int i = 0; i<n; i++)
+        int x = 0, y = 0;
+        for(int i = 0; i<s.size(); i++)
         {
-            if(s[i]-'0' != i%2)
-                ans++;
+            // 1st case 1010, evens = 1 and odds = 0
+            if(i%2 == 0 && s[i] == '1')
+            {
+                x++;
+            }
+            else if(i%2 == 1 && s[i] == '0')
+            {
+                x++;
+            }
+            // 2nd case 0101, evens = 0 and odds = 1
+            if(i%2 == 0 && s[i] == '0')
+            {
+                y++;
+            }
+            else if(i%2 == 1 && s[i] == '1')
+            {
+                y++;
+            }
         }
-        return min(ans, n - ans);
+        return min(x, y);
     }
 };
