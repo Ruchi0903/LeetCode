135876c175b68b9a9e72c9377da5a649af0b18bc
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2023-10-27 00:15:57 +0530

    Time: 0 ms (100%), Space: 9.5 MB (56.46%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        long long firstMax = LLONG_MIN;
-        long long secondMax = LLONG_MIN;
-        long long thirdMax = LLONG_MIN;
-
-        for (const int num : nums) {
-            if (num == firstMax || num == secondMax || num == thirdMax) {
-                continue;  // Skip duplicates
-            }
-
-            if (num > firstMax) {
-                thirdMax = secondMax;
-                secondMax = firstMax;
-                firstMax = num;
-            } else if (num > secondMax) {
-                thirdMax = secondMax;
-                secondMax = num;
-            } else if (num > thirdMax) {
-                thirdMax = num;
-            }
+        sort(nums.begin(), nums.end());
+        int count = 1;
+        for(int i = nums.size()-2; i>=0; i--)
+        {
+            if(nums[i] < nums[i+1])
+                count++;
+            if(count == 3)
+                return nums[i];
         }
-
-        if (thirdMax != LLONG_MIN) {
-            return thirdMax;
-        } else {
-            return firstMax;  // If there is no third max, return the first max
-        }
+        return nums[nums.size()-1]; // return the max no if there is no 3rd max no.
     }
 };
