f3549eecbc51e0458a8fc2a6bbcba93f6e331e1e
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2024-02-07 23:43:43 +0530

    Time: 3 ms (41.72%) | Memory: 8 MB (16.09%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
+    double solve(double x, int n)
+    {
+        if(n==0) return 1;
+
+        double temp = myPow(x, n/2);
+        temp = temp*temp;
+        if(n&1) return temp*x;
+        else return temp;
+    }
     double myPow(double x, int n) {
-        if(n==0){
-            return 1;
-        }
-        if(n<0){
-            n = abs(n);
-            x = 1/x;
-        }
-        if(n%2==0){
-            return myPow(x*x, n/2);
-        }
-        else{
-            return x*myPow(x*x, n/2);
-        }
+        double res = solve(x, abs(n));
+        if(n<0) return 1/res;   // checking if n = odd.
+         return res;    // here checking if n = even.
     }
 };
